292ad3221239a62674854e6789ac422dd836852a
DavidCardonaG <softwarewd@outlook.com>
2021-09-28 23:01:41 -0500

    initial app

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const App = () => {
+    return (
+        <div>
+            <h1 className="title">AMAZONAS</h1>
+            <p className="description">Design By David</p>
+            
+        </div>
+    )
+}
+
+export default App;
